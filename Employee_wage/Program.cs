5f2020257bf4cf8b5c054072de56c159cf363cef
VivekG11 <vivekvk2903@gmail.com>
2021-06-30 18:09:17 +0530

    [Calculating Salary for n num of days]

--- Employee_wage/Program.cs
+++ Employee_wage/Program.cs
@@ -7,13 +7,18 @@ namespace Employee_wage
         public const int isFullTime = 1;
         public const int isPartTime = 2;
         public const int wagePerHour = 20;
+        public const int numOfWorkingDays = 13;
         static void Main(string[] args)
         {
             //Initialising Variables
             int empHours = 0;
             int empWage = 0;
-            Random random = new Random();
-            int empAttn = random.Next(0, 3);
+            int totalWage = 0;
+            for (int day = 0; day < numOfWorkingDays; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+            /*int empAttn = random.Next(0, 3);
             if (empAttn == 1)
             {
                 Console.WriteLine("Employee is Present");
@@ -22,8 +27,8 @@ namespace Employee_wage
             {
                 Console.WriteLine("Employee is Absent");
             }
-            /*...................................................................
-             * Calculating employee wage based on working hours*/
+            *...................................................................
+             * Calculating employee wage based on working hours
             int empCheck = random.Next(0, 3);
             if (empCheck == isFullTime)
             {
@@ -43,20 +48,24 @@ namespace Employee_wage
             /*.................................................................
              * solving wage using switch case statement
              * .........................................................*/
-            switch(empCheck)
-            {
-                case isFullTime:
-                    empHours = 8;
-                    break;
-                case isPartTime:
-                    empHours = 4;
-                    break;
-                default:
-                    empHours = 0;
-                    break;
+          
+                switch (empCheck)
+                {
+                    case isFullTime:
+                        empHours = 8;
+                        break;
+                    case isPartTime:
+                        empHours = 4;
+                        break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+                empWage = empHours * wagePerHour;
+                totalWage += empWage;
+                Console.WriteLine("Employee Wage is :" + empWage);
             }
-            empWage = empHours * wagePerHour;
-            Console.WriteLine("Employee Wage is :" + empWage);
+            Console.WriteLine("Total wage of employee is : "+ totalWage);
         }
     }
 }
