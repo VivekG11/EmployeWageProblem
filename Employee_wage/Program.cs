d8445953ff8987ed9a073d99333661aad22db7f4
VivekG11 <vivekvk2903@gmail.com>
2021-06-30 13:34:08 +0530

    [Employee Attendance]

---
+++ Employee_wage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Employee_wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            int isAbsent = 0;
+            Random random = new Random();
+            int empAttn = random.Next(0, 2);
+            if (empAttn == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
