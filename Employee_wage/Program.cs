d5da50a7efe2d7f0aea86984335961773adf442c
VivekG11 <vivekvk2903@gmail.com>
2021-06-30 14:16:18 +0530

    [WageCalculation]

--- Employee_wage/Program.cs
+++ Employee_wage/Program.cs
@@ -6,8 +6,13 @@ namespace Employee_wage
     {
         static void Main(string[] args)
         {
+            //Initialising Variables
             int isPresent = 1;
             int isAbsent = 0;
+            int isFullTime = 1;
+            int wagePerHour = 20;
+            int empHours = 0;
+            int empWage = 0;
             Random random = new Random();
             int empAttn = random.Next(0, 2);
             if (empAttn == 1)
@@ -18,12 +23,8 @@ namespace Employee_wage
             {
                 Console.WriteLine("Employee is Absent");
             }
-            //...................................................................
-            int isFullTime = 1;
-            int wagePerHour = 20;
-            int empHours = 0;
-            int empWage = 0;
-            Random random = new Random();
+            /*...................................................................
+             * Calculating employee wage based on working hours*/
             int empCheck = random.Next(0, 2);
             if (empCheck == isFullTime)
             {
