23dd7ba7762c4fa40d8a7cf585916d7742b5c1c2
VivekG11 <vivekvk2903@gmail.com>
2021-07-06 13:01:01 +0530

    [Computing wages for multiple companies]

---
+++ Employee_wage/EmpWageBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_wage
+{
+    class EmpWageBuilder
+    {
+    }
+}
--- Employee_wage/Program.cs
+++ Employee_wage/Program.cs
@@ -1,67 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Employee_wage
 {
-    class WageBuilder
+    class Program
     {
-
-        public string companyName;
-        public const int isFullTime = 1;
-        public const int isPartTime = 2;
-        public int wagePerHour ;
-        public int numOfWorkingDays;
-        public int maxWorkingHours;
-
-
-
-        public WageBuilder(String companyName , int wagePerHour, int numOfWorkingDays, int maxWorkingHours )
+        public static void Main(string[] args)
         {
-            this.companyName = companyName;
-            this. wagePerHour = wagePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxWorkingHours = maxWorkingHours;
-        }
-        public void ComputeWage()
-        {
-            //Initialising Variables
-            int empHours = 0;
-            int totWorkingHours = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
-            int totalWorkingDays = 0;
-
-
-            Random random = new Random();
-            while (totWorkingHours < maxWorkingHours && totalWorkingDays < numOfWorkingDays)
-            {
-               
-                
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case isFullTime:
-                        empHours = 8;
-                        break;
-                    case isPartTime:
-                        empHours = 4;
-                        break;
-                    default:
-                        empHours = 0;
-                        break;
-                }
-                totalWorkingDays++;
-                totWorkingHours += empHours;
-            }
-            Console.WriteLine("working Days :" + totalWorkingDays + " and total working hours are : " + totWorkingHours);
-             totalEmpWage = totWorkingHours * wagePerHour;
-            Console.WriteLine("Total wage of employee is : "+ totalEmpWage);
-        }
-        static void Main(string[] args)
-        {
-            WageBuilder accenture = new WageBuilder("Accenture", 50, 22, 130);
-            WageBuilder cognizant = new WageBuilder("Cognizant", 40, 24, 130);
+            EmpWageBuilder accenture = new EmpWageBuilder("Accenture", 50, 22, 130);
+            EmpWageBuilder cognizant = new EmpWageBuilder("Cognizant", 40, 24, 130);
             accenture.ComputeWage();
             cognizant.ComputeWage();
         }
     }
+    
 }
