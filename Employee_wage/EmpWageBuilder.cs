764fabc58d772269f0cb54272b4b4234b97c1da4
VivekG11 <vivekvk2903@gmail.com>
2021-07-06 18:18:45 +0530

    [Using Interface Approach]

--- Employee_wage/EmpWageBuilder.cs
+++ Employee_wage/EmpWageBuilder.cs
@@ -5,7 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Employee_wage
-{
+{ 
+    public interface IEmployeWage
+    {
+        void addCompanyEmpWage(String companyName, int wagePerHour, int numOfWorkingDays, int maxWorkingHourse);
+    }
     class EmpWageBuilderArray
     {
         public const int isFullTime = 1;
